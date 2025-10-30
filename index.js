23ecb1802346bb015c6023bf2122f704569b31ad
Natsukiraa <gazio.mael@gmail.com>
2025-10-30 14:52:12 +0100

    feat: bonus passes, but regression on other tests

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+export const Figure = {
+  YAMS: 'yams',
+  FULL: 'full',
+  CARRE: 'carre',
+  BRELAN: 'brelan',
+  SUITE: 'suite',
+  CHANCE: 'chance',
+};
+
+const figures = {
+  [Figure.YAMS]: 50,
+  [Figure.FULL]: 30,
+  [Figure.CARRE]: 35,
+  [Figure.BRELAN]: 28,
+  [Figure.SUITE]: 40,
+  [Figure.CHANCE]: null,
+};
+
+
+
 const isSuite = (dices) => {
   const sortedDices = [...dices].sort((a, b) => a - b);
 
@@ -10,11 +30,12 @@ const isSuite = (dices) => {
   return true;
 }
 
-export const computeYamsSuite = (dices) => {
+export const computeYamsSuite = (dices, alreadyUsedFigures = new Set()) => {
   if(dices.length !== 5) {
     throw new Error('Invalid number of dices');
   }
   let dicesValues = {};
+  
 
   dices.forEach((dice) => {
     if(typeof dice !== 'number' || dice < 1 || dice > 6) {
@@ -29,21 +50,48 @@ export const computeYamsSuite = (dices) => {
 
   const counts = Object.values(dicesValues);
 
-  if(counts.includes(5)) return 50;
-  if(counts.includes(2) && counts.includes(3)) return 30;
-  if(counts.includes(4)) return 35;
-  if(counts.includes(3)) return 28;
-  if(isSuite(dices)) return 40;
-  
-  return dices.reduce((a,b) => a + b, 0);
+  if(counts.includes(5) && !alreadyUsedFigures.has(Figure.YAMS)) {
+    console.log('Using YAMS');
+    alreadyUsedFigures.add(Figure.YAMS);
+    return figures[Figure.YAMS];
+  }
+  if(counts.includes(2) && counts.includes(3) && !alreadyUsedFigures.has(Figure.FULL)) {  
+    console.log('Using FULL', alreadyUsedFigures);
+    alreadyUsedFigures.add(Figure.FULL);
+    return figures[Figure.FULL];
+  }
+  if(counts.includes(4) && !alreadyUsedFigures.has(Figure.CARRE)) {
+    console.log('Using CARRE');
+    alreadyUsedFigures.add(Figure.CARRE);
+    return figures[Figure.CARRE];
+  }
+  if(counts.includes(3) && !alreadyUsedFigures.has(Figure.BRELAN)) {
+    console.log('Using BRELAN');
+    alreadyUsedFigures.add(Figure.BRELAN);
+    return figures[Figure.BRELAN];
+  }
+  if(isSuite(dices) && !alreadyUsedFigures.has(Figure.SUITE)) {
+    console.log('Using SUITE');
+    alreadyUsedFigures.add(Figure.SUITE);
+    return figures[Figure.SUITE];
+  }
+
+  if(!alreadyUsedFigures.has(Figure.CHANCE)) {
+    console.log('Using CHANCE');
+    alreadyUsedFigures.add(Figure.CHANCE);
+    return dices.reduce((a,b) => a + b, 0);
+  }
+
+  return 0;
   
 }
 
 export const computeGameScore = (dicesThrows) => {
   let totalScore = 0;
+  let alreadyUsedFigures = new Set();
   try {
     dicesThrows.forEach((diceThrow) => {
-        totalScore += computeYamsSuite(diceThrow);
+        totalScore += computeYamsSuite(diceThrow, alreadyUsedFigures);
     });
   } catch (e) {
     throw new Error('Invalid dice throw');
