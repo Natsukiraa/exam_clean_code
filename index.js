8f8a0e194cf25d60c0eaebd83c97ef571f53d481
Natsukiraa <gazio.mael@gmail.com>
2025-10-30 14:25:56 +0100

    feat: suite test passed

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const isSuite = (dices) => {
+  const sortedDices = dices.sort();
+  const suite1 = [1,2,3,4,5];
+  const suite2 = [2,3,4,5,6];
+
+  for (let i = 0; i < sortedDices.length; i++) {
+    if (sortedDices[i] !== suite1[i] && sortedDices[i] !== suite2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 export const computeYamsSuite = (dices) => {
   let dicesValues = {};
 
@@ -14,5 +28,6 @@ export const computeYamsSuite = (dices) => {
   if(counts.includes(2) && counts.includes(3)) return 30;
   if(counts.includes(4)) return 35;
   if(counts.includes(3)) return 28;
+  if(isSuite(dices)) return 40;
   
 }
