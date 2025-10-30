c63a83267c7f89cacb61a5e55f16709edc696ba1
Natsukiraa <gazio.mael@gmail.com>
2025-10-30 14:16:34 +0100

    test: basic suite tests

---
+++ index.test.js
@@ -0,0 +1,15 @@
+import { describe,it,expect } from "vitest"
+
+
+describe('Yams basic suite tests', () => {
+  it.each([
+    [[1,1,1,2,3], 28],
+    [[1,1,1,1,2], 35],
+    [[1,1,1,2,2], 30],
+    [[1,2,3,4,5], 40],
+    [[5,5,5,5,5], 50],
+    [[3,2,1,4,6], 18],
+  ])("should return %s given %s", (input, expected) => {
+    expect(print(input)).toBe(expected);
+  })
+})
